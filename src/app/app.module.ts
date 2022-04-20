485f123dd089eac58cdbcc953fbf4ad1325cec91
James Kaloki <Kaloki@jannettes-MacBook-Air.local>
2022-04-20 13:26:19 +0300

    components for applications and bootstrap added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { MainpageComponent } from './mainpage/mainpage.component';
+import { FooterComponent } from './footer/footer.component';
+import { RepositorySearchComponent } from './repository-search/repository-search.component';
+import { UserSearchComponent } from './user-search/user-search.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    MainpageComponent,
+    FooterComponent,
+    RepositorySearchComponent,
+    UserSearchComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
