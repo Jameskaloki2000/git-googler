23528e2f1c3272b42e3ba4aeea3b3b83790d67fd
James Kaloki <Kaloki@jannettes-MacBook-Air.local>
2022-04-20 13:48:18 +0300

    created  two fucntion in mainpage component getuserdetails and getuderepositories also imported class user to be used in mainpage component

--- src/app/mainpage/mainpage.component.ts
+++ src/app/mainpage/mainpage.component.ts
@@ -1,15 +1,44 @@
 import { Component, OnInit } from '@angular/core';
+import { RepositoryUserService } from '../repository-user.service';
+import { User } from '../user';
 
 @Component({
   selector: 'app-mainpage',
   templateUrl: './mainpage.component.html',
-  styleUrls: ['./mainpage.component.css']
+  styleUrls: ['./mainpage.component.css'],
 })
 export class MainpageComponent implements OnInit {
-
-  constructor() { }
+  user!: User;
+  repositories: any;
+  constructor(private repositoryUserService: RepositoryUserService) {}
 
   ngOnInit(): void {
+    this.getUserDetails('James');
+    this.getUserRepositories('Kaloki');
   }
 
+  //user details
+  getUserDetails(githubUsername: string) {
+    this.repositoryUserService.getUserResponse(githubUsername).then(
+      (response) => {
+        this.user = this.repositoryUserService.getUserDetails;
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
+
+  //user repositories
+  getUserRepositories(githubUsername: string) {
+    this.repositoryUserService.getRepositoryResponse(githubUsername).then(
+      (response) => {
+        this.repositories = this.repositoryUserService.getRepositoryDetails;
+        console.log(this.repositories);
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
 }
